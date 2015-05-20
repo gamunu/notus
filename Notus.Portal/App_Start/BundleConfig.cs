31d7aa7b387aa5509a973e66c82ed4a07ca919ad
Gamunu Balagalla <gamunuud@gmail.com>
2015-05-20 11:53:03 +0530

    Added source code to the git repository, added new .gitingore

---
+++ Notus.Portal/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Optimization;
+
+namespace Notus.Portal
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Content/js/jquery-1.10.2.js",
+                "~/Content/js/jquery-ui-1.10.4.custom.min.js",
+                "~/Content/js/bootstrap.js",
+                "~/Content/js/gsdk-checkbox.js",
+                "~/Content/js/gsdk-radio.js",
+                "~/Content/js/gsdk-bootstrapswitch.js",
+                "~/Content/js/get-shit-done.js",
+                "~/Content/js/custom.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/get-shit-done.css",
+                "~/Content/css/pe-icon-7-stroke.css",
+                "~/Content/css/ct-navbar.css",
+                //Font awesome css
+                "~/Content/css/font-awesome.css"));
+        }
+    }
+}
---
+++ Notus.Portal/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Notus.Portal.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Notus.Portal.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Notus.Portal.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Notus.Portal.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
