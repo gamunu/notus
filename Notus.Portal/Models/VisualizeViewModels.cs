397cd1f67081efe67ed57f51ccc01d54af7ac888
Gamunu Balagalla <gamunuud@gmail.com>
2015-05-27 23:06:15 +0530

    ChartControlViewModel getter setter to make labels work

--- Notus.Portal/Models/VisualizeViewModels.cs
+++ Notus.Portal/Models/VisualizeViewModels.cs
@@ -10,30 +10,30 @@ namespace Notus.Portal.Models
     {
         [Required]
         [Display(Name = "Cause of Disease or Injury")]
-        public string CauseOf;
+        public string CauseOf { get; set; }
 
         [Required]
         [Display(Name = "Metric")]
-        public string Metric;
+        public string Metric { get; set; }
 
         [Required]
         [Display(Name = "Place")]
-        public string Place;
+        public string Place { get; set; }
 
         [Required]
         [Display(Name = "Year")]
-        public string Year;
+        public string Year { get; set; }
 
         [Required]
         [Display(Name = "Age")]
-        public string Age;
+        public string Age { get; set; }
 
         [Required]
         [Display(Name = "Depth")]
-        public string Depth;
+        public string Depth { get; set; }
 
         [Required]
         [Display(Name = "Color")]
-        public string Color;
+        public string Color { get; set; }
     }
 }
